8f0f1a98398b332fda657c9aa4a0468c4c2f763e
fabi47 <fabi.47jsc@icloud.com>
2020-07-08 09:11:13 +0900

    first

---
+++ switch.php
@@ -0,0 +1,12 @@
+touch switch.php
+
+<?php
+
+    
+        
+$name = "大泉";
+
+echo $name."さん";
+
+josui47SC
+?>
---
+++ var.php
@@ -0,0 +1,10 @@
+<?php
+
+function check($num) {
+
+ if($num == 42) {
+    echo "Answer to the Ultimate Question of Life, the Universe, and Everything";
+}
+}
+check(42);
+?>
